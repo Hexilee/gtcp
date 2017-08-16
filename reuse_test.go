185021373f3a743360d8d3d3e8eb8da9e5807b5a
Pythonify <qingxuanshabao@qq.com>
2017-08-16 14:00:00 +0800

    test fail, seems somewhere block

--- reuse_test.go
+++ reuse_test.go
@@ -1,2 +1,23 @@
 package gtcp
 
+import (
+	"testing"
+	"context"
+)
+
+func doAllTest(t *testing.T) {
+	TestTCPCtrlInterface_Server(t)
+	TestTCPCtrlInterface_Client(t)
+	TestTCPConnInterface(t)
+	TestTCPConn(t)
+	TestTCPConn_InstallCtx(t)
+}
+
+func TestConnPool(t *testing.T) {
+	InitPool(30)
+	doAllTest(t)
+	ReInitPoolWithCtx(100, context.Background())
+	doAllTest(t)
+	OpenPool()
+	doAllTest(t)
+}
